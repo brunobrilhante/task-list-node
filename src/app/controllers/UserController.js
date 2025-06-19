db610988e35993d02a5d34346f1be993bdd204ed
Bruno Brilhante <brunobrilhante53@gmail.com>
2025-06-19 16:28:55 -0300

    implemeting jwt and auth middleware

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,4 +1,4 @@
-import User from "../app/models/User";
+import User from "../models/User";
 
 class UserController {
   async store(req, res) {
@@ -17,6 +17,11 @@ class UserController {
       id,
       name,
       email })
+  }
+
+  async update(req, res) {
+    console.log(req.userId)
+    return res.json({ok:true})
   }
 }
 
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd1efc5e4fda0365a0605261c65737c1d',
+  expiresIn: '7d'
+}
